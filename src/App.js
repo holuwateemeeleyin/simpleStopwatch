bd3ccd023e70f1116ce9cbbb36755fb84f1bfc1b
Samuel Oluwatimileyin Abegunde <abegundeosamuel@gmail.com>
2022-06-25 12:45:53 +0100

    Timer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useRef, useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const timerId = useRef(0);
+  const [count, setCount] = useState(0);
+
+  const startTimer = () => {
+    timerId.current = setInterval(() => setCount((c) => c + 1), 1000);
+  };
+  const stopTimer = () => {
+    clearInterval(timerId.current);
+  };
+  const resetTimer = () => {
+    setCount(0);
+  };
+  console.log("console test");
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <p>Timer: {count} </p>
+      <button onClick={startTimer}> Start </button>
+      <button onClick={resetTimer}> Reset </button>
+      <button onClick={stopTimer}> Stop </button>
     </div>
   );
 }
